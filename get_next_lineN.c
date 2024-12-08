eee5167d5dec8da263dde30a293ce0b26a53b5b3
terzogode <mbrighi@student.42firenze.it>
2024-12-08 22:53:34 +0100

    Add files via upload

---
+++ get_next_lineN.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrighi <mbrighi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 17:12:45 by mbrighi           #+#    #+#             */
+/*   Updated: 2024/12/08 21:49:25 by mbrighi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+#endif
+/* 
+char ft_trasla(char *line, char *buf)
+{
+	char	temp;
+
+	temp = *ft_substr(buf, 0, (size_t) ft_strchr(buf, '\n'));
+	line = ft_strjoingnl(line, &temp);
+	return (*line);
+}
+
+char	ft_recalloc(char *buf, int fd)
+{
+	char	*line;
+	int 	bytes_read;
+	
+	line = NULL;
+	bytes_read = BUFFER_SIZE;
+	if (ft_strlen(buf) > 0)
+		line = ft_strjoingnl(line, buf);
+	while (bytes_read == BUFFER_SIZE && ft_strchr(buf, '\n'))
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(line);
+			return (0);
+		}
+		if (bytes_read == 0)
+			return (*line);
+		buf[bytes_read] = 0;
+		if(ft_strchr(buf, '\n'))
+			line = ft_strjoingnl(line, buf);
+		else
+			ft_trasla(line, buf);
+	}	
+	return (*line);
+} */
+
+char	*get_next_line(int fd)
+{
+    static char	buf[BUFFER_SIZE];
+    ssize_t		ft_read;
+    
+    ft_read = read(fd, buf, BUFFER_SIZE);
+    if (ft_read > 0)
+    {
+        buf[ft_read] = '\0'; // Assicurati che la stringa sia terminata
+        printf("%s", buf);
+    }
+    else
+    {
+        printf("Errore nella lettura o fine del file.\n");
+    }
+
+    return (0);
+}
+
+int	main()
+{
+    int			fd;
+
+    fd = open("prova", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Errore nell'apertura del file");
+        return (1);
+    }
+    get_next_line(fd);
+    close(fd);
+    
+    return (0);
+}
+
+
+
